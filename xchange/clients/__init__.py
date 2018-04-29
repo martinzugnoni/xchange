3a60ef9cf0f91bcf0d5bc5479e0c22f50e48b54f
Martin Zugnoni <martin.zugnoni@gmail.com>
2018-04-29 11:46:47 -0300

    Add .coveragerc file

--- xchange/clients/__init__.py
+++ xchange/clients/__init__.py
@@ -1,5 +1,4 @@
 from xchange.clients.bitfinex import BitfinexClient
-# from xchange.clients.kraken import KrakenClient
 from xchange.clients.okex import OkexClient
 
 __all__ = [BitfinexClient, OkexClient]
